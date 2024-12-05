bc7898014981f16870281e77b3ec963c482ac638
itzznivi <118966067+itzznivi@users.noreply.github.com>
2024-12-05 17:13:53 +0530

    env changes

--- src/GlobalConstants.tsx
+++ src/GlobalConstants.tsx
@@ -63,7 +63,8 @@ const userType = determineUserType();
 console.log("Final Determined User Type:", userType);
 
 const GLOBAL_CONSTANTS = {
-  BACKEND_API_URL: "http://localhost:3001/",
+  // BACKEND_API_URL: "http://localhost:3001/",
+  BACKEND_API_URL: "http://20.197.4.12:3001/",
 
   url: import.meta.env.VITE_BACKEND_API_URL as string,
   fileUrl: import.meta.env.VITE_BACKEND_API_URL as string,
